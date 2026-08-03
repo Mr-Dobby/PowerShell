754da20d3277a645d6a468c414ea349f07a9d719
Dobby <help@serverelf.com>
2026-08-03 15:53:11 +0200

    onedrive removal

--- Win11-Provisioning/Modules/OneDrive.psm1
+++ Win11-Provisioning/Modules/OneDrive.psm1
@@ -8,6 +8,7 @@ function Disable-OneDriveAutoStart {
     Write-Log 'Disabling OneDrive autostart' 'INFO'
 
     Set-RegistryDword -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Value 1
+    Set-ItemProperty -Path $PolicyPath -Name "DisableFileSync" -Value 1 -Type DWord
 
     $runPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
     if (Test-Path -Path $runPath) {
@@ -21,7 +22,16 @@ function Uninstall-OneDrive {
 
     Write-Log 'Uninstalling OneDrive' 'INFO'
 
+    Stop-Process -Name "OneDrive*" -Force -ErrorAction SilentlyContinue
+    if (Test-Path "$env:SystemRoot\SysWOW64\OneDriveSetup.exe") {
+        Start-Process "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" -ArgumentList "/uninstall" -Wait
+    } else {
+        Start-Process "$env:SystemRoot\System32\OneDriveSetup.exe" -ArgumentList "/uninstall" -Wait
+    }
+
     Get-Process OneDrive -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+    Remove-Item "Registry::HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -Force -ErrorAction SilentlyContinue
+    remove-Item "Registry::HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -Force -ErrorAction SilentlyContinue
 
     $candidates = @(
         "$env:SystemRoot\System32\OneDriveSetup.exe",
